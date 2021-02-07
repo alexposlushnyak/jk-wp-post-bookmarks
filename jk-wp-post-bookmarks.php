ffc642563ad14245f9d2518c5e7b059c1fcae11e
Alexander Poslushnyak <fr4nk.horr1gan@gmail.com>
2021-02-08 00:09:31 +0200

    Added render method for bookmark button

--- jk-wp-post-bookmarks.php
+++ jk-wp-post-bookmarks.php
@@ -3,11 +3,57 @@
 class jk_wp_post_bookmarks
 {
 
-    public function init()
+    public static function the_bookmark_button($id, $user_id)
     {
 
+        $is_user_logged_in = is_user_logged_in();
 
+        $classes = array();
 
+        if ($is_user_logged_in):
+
+            $active = false;
+
+            $bookmarks = get_option('bookmarks_user_' . $user_id);
+
+            if (empty($bookmarks)):
+
+                $bookmarks = array();
+
+            endif;
+
+            if (in_array($id, $bookmarks)):
+
+                $active = true;
+
+            endif;
+
+            if ($active):
+
+                array_push($classes, 'bookmark-active');
+
+            endif;
+
+        else:
+
+            array_push($classes, 'login-modal-trigger');
+
+            array_push($classes, 'not-authorize');
+
+        endif;
+
+        ?>
+
+        <!-- Bookmark button -->
+        <div class="bookmark-button <?php echo esc_attr(implode(' ', $classes)); ?>"
+             data-post-id="<?php echo esc_attr($id); ?>">
+
+            <!-- Bookmark icon -->
+            <i class="far fa-bookmark"></i>
+
+        </div>
+
+        <?php
     }
 
 }
